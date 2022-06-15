8fcfb8952ca8e6714ca2aca4245a0f5299e7283c
puneet2121 <bedipuneet21@gmail.com>
2022-06-15 16:45:30 -0600

    build button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
+import { portalSuspended } from "pg-protocol/dist/messages";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = classNames({
+      'button':true,
+      'button--confirm':props.confirm,
+      'button--danger':props.danger
+  
+});
+   return (
+      <button disabled={props.disabled} 
+      onClick={props.onClick} 
+      className={buttonClass}>{props.children}</button>
+   )
 }
