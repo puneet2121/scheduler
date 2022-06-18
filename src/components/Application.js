3db0652ddb785fb54128e98910e7aca8bcccb61e
puneet2121 <bedipuneet21@gmail.com>
2022-06-18 15:55:10 -0600

    Add api axios instead of hardcoded

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect,useState } from "react";
+import axios from "axios";
 import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
@@ -42,23 +43,7 @@ const appointments = {
   }
 };
 export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState('Monday');
   const updateDay = (d) => {
     console.log(d);
@@ -66,7 +51,14 @@ export default function Application(props) {
 
     
   }
-
+  useEffect(() => {
+    const url = `http://localhost:8001/api/days`
+    axios.get(url).then(response => {
+      setDays(() => 
+        [...response.data]
+      )
+    },[])
+  })
   const appointmentList = Object.values(appointments).map((appointment) => {
     return (
       <Appointment 
