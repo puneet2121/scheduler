52474f374f47723ce3db304e22b521d9b31bd576
puneet2121 <bedipuneet21@gmail.com>
2022-06-22 20:49:56 -0600

    Add test files to the scheduler

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,17 +7,33 @@ import React, { useState } from 'react';
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer ? props.interviewer.id : null);
+  const [error, setError] = useState("");
   const reset = () => {
     setStudent('')
     setInterviewer(null)
   }
   const cancel =() => {
     reset();
+    setError("");
     props.onCancel()
   }
+  const validate = () => {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    setError("");
+    props.onSave(student, interviewer);
+  }
+ 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
+      
         <form autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
@@ -26,18 +42,24 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
+            
           />
+          <section className="appointment__validation">{error}</section>
         </form>
+        
         <InterviewerList 
+        
         interviewers={props.interviewers}
         value={interviewer}
         onChange={setInterviewer}
     />
       </section>
+     
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel} >Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student,interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
 import './InterviewerList.scss'
+import PropTypes from 'prop-types'; 
 
 export default function InterviewerList(props) {
   const interviewerList = props.interviewers.map((interviewer) => {
@@ -15,10 +16,15 @@ export default function InterviewerList(props) {
     )
 
   });
+  
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">interviewer</h4>
       <ul className="interviewers__list">{interviewerList}</ul>
     </section>
   );
+  
 }
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
