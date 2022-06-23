ffbc72a5bf86f72036721d3d9114280be0c02155
puneet2121 <bedipuneet21@gmail.com>
2022-06-23 15:35:24 -0600

    final code

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,9 +1,7 @@
-describe("Add apointments", () => {
+describe("Apointments(Add,cancel,edit)", () => {
   beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
-
     cy.visit("/");
-
     cy.contains("Monday");
   });
 
@@ -23,21 +21,16 @@ describe("Add apointments", () => {
       .click({ force: true });
     cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
     cy.get("[alt='Tori Malcolm']").click();
-
     cy.contains("Save").click();
-
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
   });
   it("Should cancel an interview", () => {
     cy.get("[alt=Delete]")
       .click({ force: true });
-
     cy.contains("Confirm").click();
-
     cy.contains("Deleting").should("exist");
     cy.contains("Deleting").should("not.exist");
-
     cy.contains(".appointment__card--show", "Archie Cohen")
       .should("not.exist");
   });
